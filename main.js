fda79caab4c1b0b4a3c034ae2cb7aaec5b732586
Steven Almeida <pyro1357@outlook.com>
2024-03-05 22:33:04 -0500

    added base functionality and interface

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
-async function getWeatherData(location) {
-    const weatherData = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=9bcfe5344ca84e389a145440240403&q=${location}&days=3`, {mode: 'cors'});
-    const weatherJSON = await weatherData.json();
-    console.log(weatherJSON);
-    return weatherJSON;
+const cityName = document.getElementById('city-name');
+const countryName = document.getElementById('country-name');
+const weather = document.getElementById('weather');
+const temperature = document.getElementById('main-temp');
+const feelsLike = document.getElementById('feels-like');
+const wind = document.getElementById('wind');
+const humidity = document.getElementById('humidity');
+const locationInput = document.getElementById('location-input');
+const searchButton = document.getElementById('search-button');
+
+let currentLocation = "";
+let tempMeasurement = "f";
+
+searchButton.addEventListener("click", () => {
+    const location = locationInput.value;
+    getWeatherData(location)
+    .then(data => {
+        updateInterface(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+});
+
+function updateInterface(data) {
+    console.log(data);
+    cityName.textContent = data.location.name;
+    if (data.location.country.match('United States of America')) {
+        countryName.textContent = data.location.region;
+    } else {
+        countryName.textContent = data.location.country;
+    }
+    weather.textContent = data.current.condition.text;
+    temperature.textContent = Math.round(+data.current[`temp_${tempMeasurement}`]);
+    feelsLike.textContent = `FEELS LIKE: ${Math.round(+data.current[`feelslike_${tempMeasurement}`])}`;
+    wind.textContent = `WIND: ${data.current.wind_mph} MPH`
+    humidity.textContent = `HUMIDITY: ${data.current.humidity}%`;
 }
 
-function fToC(f) {
-    return (f - 32) * 5/9;
+async function getWeatherData(location) {
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=9bcfe5344ca84e389a145440240403&q=${location}&days=3`, {mode: 'cors'});
+        if (response.status === 400) {
+            throw new Error("Couldn't find location: " + location);
+        }
+        const weatherData = await response.json();
+        return weatherData;
+    }
+    catch (error) {
+        throw new Error("Couldn't find location: " + location);
+    }
 }
 
 getWeatherData("ireland");
