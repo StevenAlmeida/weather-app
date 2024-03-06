23c734b394b2f1c7fc456abb9b055b4c13fee819
Steven Almeida <pyro1357@outlook.com>
2024-03-06 16:26:36 -0500

    Added the ability to toggle between farenheit and celsius by clicking the current temp

--- main.js
+++ main.js
@@ -7,12 +7,18 @@ const wind = document.getElementById('wind');
 const humidity = document.getElementById('humidity');
 const locationInput = document.getElementById('location-input');
 const searchButton = document.getElementById('search-button');
-const body = document.querySelector("body");
+const body = document.querySelector('body');
+const form = document.querySelector('form');
 
-let currentLocation = "";
-let tempMeasurement = "f";
+let currentLocation = '';
+let tempIndex = false;
+let temps = {
+    main: [],
+    feelslike: []
+};
 
-searchButton.addEventListener("click", () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
     const location = locationInput.value;
     getWeatherData(location)
     .then(data => {
@@ -23,6 +29,20 @@ searchButton.addEventListener("click", () => {
     });
 });
 
+temperature.addEventListener('click', () => {
+    updateTemps(true);
+});
+
+function updateTemps(toggle) {
+    if (toggle) {
+        tempIndex = !tempIndex;
+        temperature.classList.toggle('celsius');
+        feelsLike.classList.toggle('celsius');
+    }
+    temperature.textContent = Math.round(+temps.main[+tempIndex]);
+    feelsLike.textContent = `FEELS LIKE: ${Math.round(+temps.feelslike[+tempIndex])}`;
+}
+
 function updateInterface(data) {
     console.log(data);
     cityName.textContent = data.location.name;
@@ -31,19 +51,18 @@ function updateInterface(data) {
     } else {
         countryName.textContent = data.location.country;
     }
+    updateTemps();
     weather.textContent = data.current.condition.text;
-    temperature.textContent = Math.round(+data.current[`temp_${tempMeasurement}`]);
-    feelsLike.textContent = `FEELS LIKE: ${Math.round(+data.current[`feelslike_${tempMeasurement}`])}`;
     wind.textContent = `WIND: ${data.current.wind_mph} MPH`
     humidity.textContent = `HUMIDITY: ${data.current.humidity}%`;
 
-    const hour = data.location.localtime.split(" ")[1].split(":")[0];
+    const hour = data.location.localtime.split(' ')[1].split(':')[0];
     if (hour > 6 && hour < 18) {
-        body.classList.add("day-clear");
-        body.classList.remove("night-clear");
+        body.classList.add('day-clear');
+        body.classList.remove('night-clear');
     } else {
-        body.classList.add("night-clear");
-        body.classList.remove("day-clear");
+        body.classList.add('night-clear');
+        body.classList.remove('day-clear');
     }
 }
 
@@ -54,6 +73,8 @@ async function getWeatherData(location) {
             throw new Error("Couldn't find location: " + location);
         }
         const weatherData = await response.json();
+        temps.main = [weatherData.current.temp_f, weatherData.current.temp_c];
+        temps.feelslike = [weatherData.current.feelslike_f, weatherData.current.feelslike_c];
         return weatherData;
     }
     catch (error) {
@@ -61,6 +82,6 @@ async function getWeatherData(location) {
     }
 }
 
-getWeatherData("Fall River").then(data => {
+getWeatherData('Fall River').then(data => {
     updateInterface(data);
 });
